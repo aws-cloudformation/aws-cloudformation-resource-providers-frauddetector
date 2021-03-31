995871f9d35d0cba32bc3bff31643502d1f0c049
James Privette <privetjr@amazon.com>
2021-03-31 11:44:03 -0400

    update boilerplate code with empty handler workers

---
+++ aws-frauddetector-detector/src/aws_frauddetector_detector/handler_workers.py
@@ -0,0 +1,25 @@
+import logging
+
+# Use this logger to forward log messages to CloudWatch Logs.
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+
+
+def execute_create_detector_handler_work(session, model, progress):
+    pass
+
+
+def execute_update_detector_handler_work(session, model, progress, request):
+    pass
+
+
+def execute_delete_detector_handler_work(session, model, progress):
+    pass
+
+
+def execute_read_detector_handler_work(session, model, progress):
+    pass
+
+
+def execute_list_detector_handler_work(session, model, progress):
+    pass
