0fc51c2fbfaf2106494c3a8c1d381656064ff705
James Privette <privetjr@amazon.com>
2021-03-31 11:44:03 -0400

    update boilerplate code with empty handler workers

--- aws-frauddetector-detector/src/aws_frauddetector_detector/handlers.py
+++ aws-frauddetector-detector/src/aws_frauddetector_detector/handlers.py
@@ -12,10 +12,12 @@ from cloudformation_cli_python_lib import (
     identifier_utils,
 )
 
+from . import handler_workers
 from .models import ResourceHandlerRequest, ResourceModel
 
 # Use this logger to forward log messages to CloudWatch Logs.
 LOG = logging.getLogger(__name__)
+LOG.setLevel(level=logging.DEBUG)
 TYPE_NAME = "AWS::FraudDetector::Detector"
 
 resource = Resource(TYPE_NAME, ResourceModel)
@@ -33,34 +35,8 @@ def create_handler(
         status=OperationStatus.IN_PROGRESS,
         resourceModel=model,
     )
-    # TODO: put code here
-
-    # Example:
-    try:
-
-        # primary identifier from example
-        primary_identifier = None
-
-        # setting up random primary identifier compliant with cfn standard
-        if primary_identifier is None:
-            primary_identifier = identifier_utils.generate_resource_identifier(
-                stack_id_or_name=request.stackId,
-                logical_resource_id=request.logicalResourceIdentifier,
-                client_request_token=request.clientRequestToken,
-                max_length=255
-                )
-
-        if isinstance(session, SessionProxy):
-            client = session.client("s3")
-        # Setting Status to success will signal to cfn that the operation is complete
-        progress.status = OperationStatus.SUCCESS
-    except TypeError as e:
-        # exceptions module lets CloudFormation know the type of failure that occurred
-        raise exceptions.InternalFailure(f"was not expecting type {e}")
-        # this can also be done by returning a failed progress event
-        # return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"was not expecting type {e}")
-
-    return read_handler(session, request, callback_context)
+    LOG.info(f"calling create with the following request: {request}")
+    return handler_workers.execute_create_detector_handler_work(session, model, progress)
 
 
 @resource.handler(Action.UPDATE)
@@ -74,8 +50,8 @@ def update_handler(
         status=OperationStatus.IN_PROGRESS,
         resourceModel=model,
     )
-    # TODO: put code here
-    return read_handler(session, request, callback_context)
+    LOG.info(f"calling update with the following request: {request}")
+    return handler_workers.execute_update_detector_handler_work(session, model, progress, request)
 
 
 @resource.handler(Action.DELETE)
@@ -89,8 +65,8 @@ def delete_handler(
         status=OperationStatus.IN_PROGRESS,
         resourceModel=None,
     )
-    # TODO: put code here
-    return progress
+    LOG.info(f"calling delete with the following request: {request}")
+    return handler_workers.execute_delete_detector_handler_work(session, model, progress)
 
 
 @resource.handler(Action.READ)
@@ -100,11 +76,12 @@ def read_handler(
     callback_context: MutableMapping[str, Any],
 ) -> ProgressEvent:
     model = request.desiredResourceState
-    # TODO: put code here
-    return ProgressEvent(
-        status=OperationStatus.SUCCESS,
+    progress: ProgressEvent = ProgressEvent(
+        status=OperationStatus.IN_PROGRESS,
         resourceModel=model,
     )
+    LOG.info(f"calling read with the following request: {request}")
+    return handler_workers.execute_read_detector_handler_work(session, model, progress)
 
 
 @resource.handler(Action.LIST)
@@ -113,8 +90,10 @@ def list_handler(
     request: ResourceHandlerRequest,
     callback_context: MutableMapping[str, Any],
 ) -> ProgressEvent:
-    # TODO: put code here
-    return ProgressEvent(
-        status=OperationStatus.SUCCESS,
-        resourceModels=[],
+    model = request.desiredResourceState
+    progress: ProgressEvent = ProgressEvent(
+        status=OperationStatus.IN_PROGRESS,
+        resourceModel=model,
     )
+    LOG.info(f"calling list with the following request: {request}")
+    return handler_workers.execute_list_detector_handler_work(session, model, progress)
