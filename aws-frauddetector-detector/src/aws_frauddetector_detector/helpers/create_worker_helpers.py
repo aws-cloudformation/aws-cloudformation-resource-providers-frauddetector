2d8b0ba469296c9cf1a77c6b0f50e74682075878
James Privette <privetjr@amazon.com>
2021-04-01 13:24:07 -0400

    bring in helpers and tests from event type
    Also update methods for eventtype to return models.EventType instead of models.ResourceModel

---
+++ aws-frauddetector-detector/src/aws_frauddetector_detector/helpers/create_worker_helpers.py
@@ -0,0 +1,109 @@
+import logging
+from cloudformation_cli_python_lib import (
+    exceptions,
+)
+
+from . import validation_helpers, common_helpers, util
+from ..models import ResourceModel
+
+# Use this logger to forward log messages to CloudWatch Logs.
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+
+
+def validate_dependencies_for_create(afd_client, model: ResourceModel):
+    _validate_event_variables_for_create(afd_client, model)
+    _validate_entity_types_for_create(afd_client, model)
+    _validate_labels_for_create(afd_client, model)
+
+
+def _validate_event_variables_for_create(afd_client, model: ResourceModel):
+    for event_variable in model.EventVariables:
+        _validate_event_variable_for_create(afd_client, event_variable)
+
+
+def _validate_event_variable_for_create(afd_client, event_variable):
+    if event_variable.Inline:
+        _validate_inline_event_variable_for_create(afd_client, event_variable)
+    else:
+        _validate_referenced_event_variable_for_create(afd_client, event_variable)
+
+
+def _validate_referenced_event_variable_for_create(afd_client, event_variable):
+    event_variable_name = util.extract_name_from_arn(event_variable.Arn)
+    get_variables_worked, _ = \
+        validation_helpers.check_if_get_variables_succeeds(afd_client, event_variable_name)
+    if not get_variables_worked:
+        raise exceptions.NotFound('event_variable', event_variable.Arn)
+
+
+def _validate_inline_event_variable_for_create(afd_client, event_variable):
+    if event_variable.Name is None:
+        raise exceptions.InvalidRequest('Error occurred: inline event variables must include Name!')
+
+    get_variables_worked, _ = \
+        validation_helpers.check_if_get_variables_succeeds(afd_client, event_variable.Name)
+    if get_variables_worked:
+        raise exceptions.AlreadyExists('event_variable', event_variable.Name)
+
+    common_helpers.create_inline_event_variable(frauddetector_client=afd_client, event_variable=event_variable)
+
+
+def _validate_entity_types_for_create(afd_client, model: ResourceModel):
+    for entity_type in model.EntityTypes:
+        _validate_entity_type_for_create(afd_client, entity_type)
+
+
+def _validate_entity_type_for_create(afd_client, entity_type):
+    if entity_type.Inline:
+        _validate_inline_entity_type_for_create(afd_client, entity_type)
+    else:
+        _validate_referenced_entity_type_for_create(afd_client, entity_type)
+
+
+def _validate_referenced_entity_type_for_create(afd_client, entity_type):
+    entity_type_name = util.extract_name_from_arn(entity_type.Arn)
+    get_entity_types_worked, _ = validation_helpers.check_if_get_entity_types_succeeds(afd_client, entity_type_name)
+    if not get_entity_types_worked:
+        raise exceptions.NotFound('entity_type', entity_type.Arn)
+
+
+def _validate_inline_entity_type_for_create(afd_client, entity_type):
+    if entity_type.Name is None:
+        raise exceptions.InvalidRequest('Error occurred: inline entity types must include Name!')
+
+    get_entity_types_worked, _ = validation_helpers.check_if_get_entity_types_succeeds(afd_client, entity_type.Name)
+    if get_entity_types_worked:
+        raise exceptions.AlreadyExists('entity_type', entity_type.Name)
+
+    common_helpers.put_inline_entity_type(afd_client, entity_type)
+
+
+def _validate_labels_for_create(afd_client, model: ResourceModel):
+    for label in model.Labels:
+        _validate_label_for_create(afd_client, label)
+
+
+def _validate_label_for_create(afd_client, label):
+    if label.Inline:
+        _validate_inline_label_for_create(afd_client, label)
+    else:
+        _validate_referenced_label_for_create(afd_client, label)
+
+
+def _validate_referenced_label_for_create(afd_client, label):
+    label_name = util.extract_name_from_arn(label.Arn)
+    get_labels_worked, _ = validation_helpers.check_if_get_labels_succeeds(afd_client, label_name)
+    if not get_labels_worked:
+        raise exceptions.NotFound('label', label.Arn)
+
+
+def _validate_inline_label_for_create(afd_client, label):
+    if label.Name is None:
+        raise exceptions.InvalidRequest('Error occurred: inline labels must include Name!')
+
+    get_labels_worked, _ = validation_helpers.check_if_get_labels_succeeds(afd_client, label.Name)
+    if get_labels_worked:
+        raise exceptions.AlreadyExists('label', label.Name)
+
+    common_helpers.put_inline_label(afd_client, label)
---
+++ aws-frauddetector-detector/src/aws_frauddetector_detector/tests/helpers/test_common_helpers.py
@@ -0,0 +1,192 @@
+from ...helpers import (
+    common_helpers,
+    model_helpers,  # for monkeypatch
+    validation_helpers  # for monkeypatch
+)
+
+from unittest.mock import MagicMock
+from .. import unit_test_utils
+
+
+def test_update_tags():
+    # Arrange
+    list_tags_response = {'tags': unit_test_utils.FAKE_TAGS}
+    fake_tag_keys = [tag.get('key', '') for tag in unit_test_utils.FAKE_TAGS]
+
+    mock_afd_client = unit_test_utils.create_mock_afd_client()
+    mock_afd_client.list_tags_for_resource = MagicMock(return_value=list_tags_response)
+    mock_afd_client.tag_resource = MagicMock()
+    mock_afd_client.untag_resource = MagicMock()
+
+    # Act
+    common_helpers.update_tags(mock_afd_client, unit_test_utils.FAKE_ARN, unit_test_utils.FAKE_TAG_MODELS_DIFFERENT)
+
+    # Assert
+    mock_afd_client.untag_resource.assert_called_once_with(resourceARN=unit_test_utils.FAKE_ARN, tagKeys=fake_tag_keys)
+    mock_afd_client.tag_resource.assert_called_once_with(resourceARN=unit_test_utils.FAKE_ARN,
+                                                         tags=unit_test_utils.FAKE_TAGS_DIFFERENT)
+
+
+def test_put_event_type_and_return_event_type_model(monkeypatch):
+    # Arrange
+    mock_afd_client, input_model, output_model = _setup_put_event_type_test()
+    fake_output_model = unit_test_utils.create_fake_event_type(True)
+    fake_output_model = _add_extra_attributes_for_event_type(fake_output_model)
+    mock_get_event_type_and_return_event_type_model = MagicMock(return_value=fake_output_model)
+
+    monkeypatch.setattr(model_helpers,
+                        'get_event_type_and_return_event_type_model',
+                        mock_get_event_type_and_return_event_type_model)
+
+    # Act/Assert
+    _act_and_assert_put_event_type_for_given_model(mock_afd_client, input_model, output_model)
+
+
+def test_put_inline_event_variable():
+    # Arrange
+    mock_afd_client = unit_test_utils.create_mock_afd_client()
+    mock_afd_client.create_variable = MagicMock()
+    fake_input_model = unit_test_utils.create_fake_event_type()
+    fake_event_variable = fake_input_model.EventVariables[0]
+    fake_tags = model_helpers.get_tags_from_tag_models(fake_event_variable.Tags)
+
+    # Act
+    common_helpers.create_inline_event_variable(mock_afd_client, fake_event_variable)
+
+    # Assert
+    mock_afd_client.create_variable.assert_called_once_with(
+        name=fake_event_variable.Name,
+        dataSource=fake_event_variable.DataSource,
+        dataType=fake_event_variable.DataType,
+        defaultValue=fake_event_variable.DefaultValue,
+        description=fake_event_variable.Description,
+        variableType=fake_event_variable.VariableType,
+        tags=fake_tags
+    )
+
+
+def test_put_inline_label():
+    # Arrange
+    mock_afd_client = unit_test_utils.create_mock_afd_client()
+    mock_afd_client.put_label = MagicMock()
+    fake_input_model = unit_test_utils.create_fake_event_type()
+    fake_label = fake_input_model.Labels[0]
+    fake_tags = model_helpers.get_tags_from_tag_models(fake_label.Tags)
+
+    # Act
+    common_helpers.put_inline_label(mock_afd_client, fake_label)
+
+    # Assert
+    mock_afd_client.put_label.assert_called_once_with(
+        name=fake_label.Name,
+        description=fake_label.Description,
+        tags=fake_tags
+    )
+
+
+def test_put_inline_entity_type():
+    # Arrange
+    mock_afd_client = unit_test_utils.create_mock_afd_client()
+    mock_afd_client.put_entity_type = MagicMock()
+    fake_input_model = unit_test_utils.create_fake_event_type()
+    fake_entity_type = fake_input_model.EntityTypes[0]
+    fake_tags = model_helpers.get_tags_from_tag_models(fake_entity_type.Tags)
+
+    # Act
+    common_helpers.put_inline_entity_type(mock_afd_client, fake_entity_type)
+
+    # Assert
+    mock_afd_client.put_entity_type.assert_called_once_with(
+        name=fake_entity_type.Name,
+        description=fake_entity_type.Description,
+        tags=fake_tags
+    )
+
+
+def _setup_update_event_variables_test(monkeypatch):
+    mock_afd_client = MagicMock()
+    input_model = unit_test_utils.create_fake_event_type()
+    input_model = _add_extra_attributes_for_event_type(input_model)
+    existing_variable_model = unit_test_utils.create_fake_event_type()
+    existing_variable_model = _add_extra_attributes_for_variable(existing_variable_model)
+    existing_variables = [existing_variable_model]
+    variable_entry_argument = unit_test_utils.create_fake_variable()
+    del variable_entry_argument['createdTime']
+    del variable_entry_argument['lastUpdatedTime']
+    del variable_entry_argument['arn']
+    missing_names = [unit_test_utils.FAKE_NAME]
+    mock_check_which_variables_exist = MagicMock(return_value=(existing_variables, missing_names))
+    mock_check_variable_differences = MagicMock(return_value={'defaultValue': True,
+                                                              'description': False,
+                                                              'variableType': False})
+    mock_get_variable_entry_argument_from_event_variable_model = \
+        MagicMock(return_value=variable_entry_argument)
+
+    monkeypatch.setattr(validation_helpers, 'check_which_variables_exist', mock_check_which_variables_exist)
+    monkeypatch.setattr(validation_helpers, 'check_variable_differences', mock_check_variable_differences)
+    monkeypatch.setattr(model_helpers, 'get_variable_entry_argument_from_event_variable_model',
+                        mock_get_variable_entry_argument_from_event_variable_model)
+
+    return mock_afd_client, input_model
+
+
+def _setup_put_event_type_test():
+    get_event_types_response = {'eventTypes': [unit_test_utils.FAKE_EVENT_TYPE]}
+    list_tags_response = {'tags': unit_test_utils.FAKE_TAGS}
+
+    mock_afd_client = unit_test_utils.create_mock_afd_client()
+    mock_afd_client.put_event_type = MagicMock()
+    mock_afd_client.get_event_types = MagicMock(return_value=get_event_types_response)
+    mock_afd_client.list_tags_for_resource = MagicMock(return_value=list_tags_response)
+
+    model = unit_test_utils.create_fake_event_type()
+    model = _add_extra_attributes_for_event_type(model)
+    expected_model = unit_test_utils.create_fake_event_type(is_output_model=True)
+    return mock_afd_client, model, expected_model
+
+
+def _add_extra_attributes_for_event_type(model):
+    fake_input_model = unit_test_utils.create_fake_event_type()
+    model.Labels = fake_input_model.Labels
+    model.EventVariables = fake_input_model.EventVariables
+    model.EntityTypes = fake_input_model.EntityTypes
+    model.EventTypeName = unit_test_utils.FAKE_NAME
+    return model
+
+
+def _add_extra_attributes_for_variable(model):
+    model.dataSource = unit_test_utils.EVENT
+    model.dataType = unit_test_utils.STRING
+    model.defaultValue = unit_test_utils.DEFAULT
+    model.variableType = unit_test_utils.EMAIL_ADDRESS
+    return model
+
+
+def _act_and_assert_put_event_type_for_given_model(afd_client, input_model, output_model):
+    result_model = common_helpers.put_event_type_and_return_event_type_model(afd_client, input_model)
+    # Need to assert each sub-attribute, otherwise full equivalence will check readonly properties too (e.g. Arn)
+    assert result_model.Name == output_model.Name
+    assert result_model.Tags == output_model.Tags
+    assert result_model.Description == output_model.Description
+    for i in range(0, len(result_model.EventVariables)):
+        actual_event_variable = result_model.EventVariables[i]
+        expected_event_variable = output_model.EventVariables[i]
+        assert actual_event_variable.Name == expected_event_variable.Name
+        assert actual_event_variable.DataSource == expected_event_variable.DataSource
+        assert actual_event_variable.DataType == expected_event_variable.DataType
+        assert actual_event_variable.DefaultValue == expected_event_variable.DefaultValue
+        assert actual_event_variable.VariableType == expected_event_variable.VariableType
+        assert actual_event_variable.Description == expected_event_variable.Description
+        assert actual_event_variable.Tags == expected_event_variable.Tags
+    for i in range(0, len(result_model.EntityTypes)):
+        actual_entity_type = result_model.EntityTypes[i]
+        expected_entity_type = output_model.EntityTypes[i]
+        assert actual_entity_type.Name == expected_entity_type.Name
+        assert actual_entity_type.Description == expected_entity_type.Description
+        assert actual_entity_type.Tags == expected_entity_type.Tags
+    for i in range(0, len(result_model.Labels)):
+        actual_label = result_model.Labels[i]
+        expected_label = output_model.Labels[i]
+        assert actual_label.Name == expected_label.Name
+        assert actual_label.Description == expected_label.Description
+        assert actual_label.Tags == expected_label.Tags
