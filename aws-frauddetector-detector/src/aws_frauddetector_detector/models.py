304e272231c1c160265357b608e5828366c61318
James Privette <privetjr@amazon.com>
2021-04-01 13:15:05 -0400

    update detector json to include event type

--- aws-frauddetector-detector/src/aws_frauddetector_detector/models.py
+++ aws-frauddetector-detector/src/aws_frauddetector_detector/models.py
@@ -46,6 +46,7 @@ class ResourceModel(BaseModel):
     Tags: Optional[Sequence["_Tag"]]
     Description: Optional[str]
     Rules: Optional[Sequence["_Rule"]]
+    EventType: Optional["_EventType"]
     Arn: Optional[str]
     CreatedTime: Optional[str]
     LastUpdatedTime: Optional[str]
@@ -67,6 +68,7 @@ class ResourceModel(BaseModel):
             Tags=deserialize_list(json_data.get("Tags"), Tag),
             Description=json_data.get("Description"),
             Rules=deserialize_list(json_data.get("Rules"), Rule),
+            EventType=EventType._deserialize(json_data.get("EventType")),
             Arn=json_data.get("Arn"),
             CreatedTime=json_data.get("CreatedTime"),
             LastUpdatedTime=json_data.get("LastUpdatedTime"),
@@ -169,3 +171,145 @@ class Outcome(BaseModel):
 
 # work around possible type aliasing issues when variable has same name as a model
 _Outcome = Outcome
+
+
+@dataclass
+class EventType(BaseModel):
+    Name: Optional[str]
+    Inline: Optional[bool]
+    Tags: Optional[Sequence["_Tag"]]
+    Description: Optional[str]
+    EventVariables: Optional[Sequence["_EventVariable"]]
+    Labels: Optional[Sequence["_Label"]]
+    EntityTypes: Optional[Sequence["_EntityType"]]
+    Arn: Optional[str]
+    CreatedTime: Optional[str]
+    LastUpdatedTime: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_EventType"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_EventType"]:
+        if not json_data:
+            return None
+        return cls(
+            Name=json_data.get("Name"),
+            Inline=json_data.get("Inline"),
+            Tags=deserialize_list(json_data.get("Tags"), Tag),
+            Description=json_data.get("Description"),
+            EventVariables=deserialize_list(json_data.get("EventVariables"), EventVariable),
+            Labels=deserialize_list(json_data.get("Labels"), Label),
+            EntityTypes=deserialize_list(json_data.get("EntityTypes"), EntityType),
+            Arn=json_data.get("Arn"),
+            CreatedTime=json_data.get("CreatedTime"),
+            LastUpdatedTime=json_data.get("LastUpdatedTime"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_EventType = EventType
+
+
+@dataclass
+class EventVariable(BaseModel):
+    Arn: Optional[str]
+    Inline: Optional[bool]
+    Name: Optional[str]
+    DataSource: Optional[str]
+    DataType: Optional[str]
+    DefaultValue: Optional[str]
+    VariableType: Optional[str]
+    Description: Optional[str]
+    Tags: Optional[Sequence["_Tag"]]
+    CreatedTime: Optional[str]
+    LastUpdatedTime: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_EventVariable"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_EventVariable"]:
+        if not json_data:
+            return None
+        return cls(
+            Arn=json_data.get("Arn"),
+            Inline=json_data.get("Inline"),
+            Name=json_data.get("Name"),
+            DataSource=json_data.get("DataSource"),
+            DataType=json_data.get("DataType"),
+            DefaultValue=json_data.get("DefaultValue"),
+            VariableType=json_data.get("VariableType"),
+            Description=json_data.get("Description"),
+            Tags=deserialize_list(json_data.get("Tags"), Tag),
+            CreatedTime=json_data.get("CreatedTime"),
+            LastUpdatedTime=json_data.get("LastUpdatedTime"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_EventVariable = EventVariable
+
+
+@dataclass
+class Label(BaseModel):
+    Arn: Optional[str]
+    Inline: Optional[bool]
+    Name: Optional[str]
+    Description: Optional[str]
+    Tags: Optional[Sequence["_Tag"]]
+    CreatedTime: Optional[str]
+    LastUpdatedTime: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_Label"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_Label"]:
+        if not json_data:
+            return None
+        return cls(
+            Arn=json_data.get("Arn"),
+            Inline=json_data.get("Inline"),
+            Name=json_data.get("Name"),
+            Description=json_data.get("Description"),
+            Tags=deserialize_list(json_data.get("Tags"), Tag),
+            CreatedTime=json_data.get("CreatedTime"),
+            LastUpdatedTime=json_data.get("LastUpdatedTime"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_Label = Label
+
+
+@dataclass
+class EntityType(BaseModel):
+    Arn: Optional[str]
+    Inline: Optional[bool]
+    Name: Optional[str]
+    Description: Optional[str]
+    Tags: Optional[Sequence["_Tag"]]
+    CreatedTime: Optional[str]
+    LastUpdatedTime: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_EntityType"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_EntityType"]:
+        if not json_data:
+            return None
+        return cls(
+            Arn=json_data.get("Arn"),
+            Inline=json_data.get("Inline"),
+            Name=json_data.get("Name"),
+            Description=json_data.get("Description"),
+            Tags=deserialize_list(json_data.get("Tags"), Tag),
+            CreatedTime=json_data.get("CreatedTime"),
+            LastUpdatedTime=json_data.get("LastUpdatedTime"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_EntityType = EntityType
