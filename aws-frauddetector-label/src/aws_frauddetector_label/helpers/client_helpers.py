52b73d272458d9dea9a96c3decc926f7a4cd7af5
James Privette <privetjr@amazon.com>
2021-04-29 11:55:57 -0400

    add boto3 standard retry config

--- aws-frauddetector-label/src/aws_frauddetector_label/helpers/client_helpers.py
+++ aws-frauddetector-label/src/aws_frauddetector_label/helpers/client_helpers.py
@@ -1,11 +1,20 @@
+from botocore.config import Config
 from cloudformation_cli_python_lib import (
     SessionProxy,
     exceptions,
 )
 
 
+BOTO3_CLIENT_CONFIG_WITH_STANDARD_RETRIES = Config(
+    retries={
+        'total_max_attempts': 3,
+        'mode': 'standard'
+    }
+)
+
+
 def get_afd_client(session):
     if isinstance(session, SessionProxy):
-        return session.client("frauddetector")
-
+        return session.client(service_name="frauddetector",
+                              config=BOTO3_CLIENT_CONFIG_WITH_STANDARD_RETRIES)
     raise exceptions.InternalFailure(f"Error: failed to get frauddetector client.")
