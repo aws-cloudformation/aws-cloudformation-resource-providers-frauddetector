80c685a8b4c7350c1fec8a48b9b9adc4b6b5a7ce
Kyle Chan <chankyl@amazon.com>
2023-01-25 14:53:30 -0500

    Implement model and validation helpers for Lists resource provider

--- aws-frauddetector-list/src/aws_frauddetector_list/helpers/validation_helpers.py
+++ aws-frauddetector-list/src/aws_frauddetector_list/helpers/validation_helpers.py
@@ -16,3 +16,19 @@ def remove_none_arguments(args):
     for key in keys_to_remove:
         del args[key]
     return args
+
+
+def check_if_get_lists_metadata_succeeds(frauddetector_client, list_name):
+    """
+    This calls get_lists_metadata and returns True if it worked, along with the API response (True, response)
+    If the call to get_labels fails, this returns (False, None)
+    :param frauddetector_client: afd boto3 client to use to make the request
+    :param label_name:  the name of the label you want to get
+    :return: a tuple: (bool, apiResponse)
+    """
+    try:
+        get_lists_metadata_response = api_helpers.call_get_lists_metadata(frauddetector_client, list_name)
+        return True, get_lists_metadata_response
+    except frauddetector_client.exceptions.ResourceNotFoundException as RNF:
+        LOG.warning(f"Error getting list {list_name}: {RNF}")
+        return False, None
